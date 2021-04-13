596724198530bf92e7216329c67e19cd73283769
GabiEscobar <80913839+GabiEscobar@users.noreply.github.com>
2021-04-13 20:23:06 +0200

    Update eixample1.py

--- eixample1.py
+++ eixample1.py
@@ -1,5 +1,5 @@
 """
-Versió1
+Versio1
 Utilitzar # per afegir comentaris (no afecten al codi)
 @author: GabiEscobar
 """
@@ -18,186 +18,156 @@ TIME_STEP = 1
 
 
 class Car():
-  
-  def __init__(self, x, y, speedX, speedY):
+  def __init__(self, x, y, speedX, speedY): 
       self.x = x
       self.y = y
       self.speedX = speedX
       self.speedY = speedY
+      self.route = []
+      self.crash = False
+      
+  def generate_route(self, map, destX, destY):
+      
+      route = []
+      oriX = self.x
+      oriY = self.y
+      
+      if not map.grid[oriX, oriY] == 0:
+          raise Exception("Origin direction is not valid.")
+      if not map.grid[destX, destY] == 0:
+          raise Exception("Destiny direction is not valid.")
+    
+      x = oriX
+      y = oriY
+      
+      while x != destX:
+          if x < destX and map.grid[x+1,y] == 0:
+              x += 1
+              route.append((x,y))
+          elif x > destX and map.grid[x-1,y] == 0:
+              x -= 1
+              route.append((x,y))
+          else:
+              break
+          
+      while y != destY:
+          if y < destY and map.grid[x,y+1] == 0:
+              y += 1
+              route.append((x,y))
+          elif x > destY and map.grid[y-1] == 0:
+              y -= 1
+              route.append((x,y))
+          else:
+              break
+         
+      self.route = route
+      print(route)
+  
+    
 
   def move(self, map):
-      newX = self.x + self.speedX*TIME_STEP
-      newY = self.y + self.speedY*TIME_STEP
       
-      if map.get(newX, newY) == 0:
-        self.x = newX
-        self.y = newY
-        
-      else:
-          print("BOOM")
-          raise
+      if len(self.route) > 0:
           
+          newX, newY = self.route[0]
+          if map.get(newX, newY) == 0:
+            self.x = newX
+            self.y = newY
+            del self.route[0]
+            return True
+            
+          else:
+              print("Un resplandor y hace: BOOM")
+              self.route = []
+              self.crash = True
+              return False
+              
+      else:
+          print("Route is empty.")
+          return False
+    
 
 class Map():
-    
     def __init__(self, sizeX, sizeY):
         self.grid = np.ones((sizeX, sizeY))
         
         #Creació de les carreterres en l'eix X
         for i in range(0, sizeX, 10): 
             self.grid[i,:] = 0
-            #self.grid[i+1,:] = 0
-            if all(self.grid[i,:]) == 0: 
-                availableX = self.grid[i,:]
         
         #Creació de les carreterres en l'eix Y
         for j in range(0, sizeY, 10): 
             self.grid[:,j] = 0
-            #self.grid[:,j+1] = 0
-            if all(self.grid[:,j]) == 0:
-                availableY = self.grid[:,j]
                 
     def show(self):
       plt.imshow(self.grid)
       
     def get(self, x, y):
       return self.grid[x,y]
+
+
+
+
+class Main():
+    def __init__(self, map, cars, dests):
+        self.map = map
+        self.cars = cars
+        for i, car in enumerate(cars):
+            car.generate_route(map, dests[i][0], dests[i][1])
+    
+    def step(self):
+        for car in self.cars:
+            prevX, prevY = car.x, car.y
+            #if car.move(self.map):
+            #    self.map.grid[prevX, prevY] = 0
+            #    self.map.grid[car.x, car.y] = 1
+            car.move(self.map)
+            
+        self.collisions() 
+
+    def show(self):
+        self.map.show()
+        for car in self.cars:
+            if not car.crash:
+                plt.scatter(car.x, car.y, c='green')
+            else:
+                plt.scatter(car.x, car.y, c='red', marker='x')
+        plt.show()
      
+    def collisions(self):
+        for car in self.cars:
+            if car.crash:
+                raise Exception(f"Un resplandor y hace: BOOM. ({car.x}, {car.y})")
+                 
+         
 
-class Game():
-  
-  def __init__(self, map, cars):
-    self.map = map
-    self.cars = cars
-    
-  def step(self):
-    for car in self.cars:
-      car.move(self.map)
-      self.collisions() 
-
-  def show(self):
-    self.map.show()
-    for car in self.cars:
-      plt.scatter(car.x, car.y, c='green')
-      self.map.grid[car.x, car.y] = 1
-      self.map.grid[car.x, car.y] = 0
-    plt.show()
-     
-  def collisions(self):  # necessita replantejament
-    for car in self.cars:
-      if self.map.grid[car.x,car.y] == 1:
-          print("BOOM")
-          raise 
-          
-
-class Node():
-    #Node for A* path finding algorithm
-    
-    def __init__(self, parent=None, position=None):
-        
-        self.parent = parent
-        self.position = position
-    
-        self.g = 0
-        self.h = 0
-        self.f = 0
-        
-
-
-def generate_route(map, oriX, oriY, destX, destY):
-    
-    origin = (oriX, oriY)
-    destiny = (destX, destY)
-    
-    origin_node = Node(None, origin)
-    origin_node.g = origin_node.h = origin_node.f = 0
-    destiny_node = Node(None, destiny)
-    destiny_node.g = destiny_node.h = destiny_node.f = 0
-    
-    open_list = []
-    closed_list = []
-    
-    
-    open_list.append(origin_node)
-    
-    if not map.get(oriX, oriY) == 0:
-        print("Origin direction is not valid.")
-        raise
-    
-    if not map.get(destX, destY) == 0:
-        print("Destiny direction is not valid.")
-        raise
-  
-    if len(open_list) == 0:
-        route = [(oriX, oriY)]
-    
-    
-    while len(open_list) > 0:
-        current_node = open_list[0]
-        current_index = 0
-        
-        for index, item in enumerate(open_list):
-            if item.f > current_node.f:
-                current_node = item
-                current_index = index
-                
-                
-
-
-
-
-"""
-// A* (star) Pathfinding
-// Initialize both open and closed list
-let the openList equal empty list of nodes
-let the closedList equal empty list of nodes
-// Add the start node
-put the startNode on the openList (leave it's f at zero)
-// Loop until you find the end
-while the openList is not empty
-    // Get the current node
-    let the currentNode equal the node with the least f value
-    remove the currentNode from the openList
-    add the currentNode to the closedList
-    // Found the goal
-    if currentNode is the goal
-        Congratz! You've found the end! Backtrack to get path
-    // Generate children
-    let the children of the currentNode equal the adjacent nodes
-    
-    for each child in the children
-        // Child is on the closedList
-        if child is in the closedList
-            continue to beginning of for loop
-        // Create the f, g, and h values
-        child.g = currentNode.g + distance between child and current
-        child.h = distance from child to end
-        child.f = child.g + child.h
-        // Child is already in openList
-        if child.position is in the openList's nodes positions
-            if the child.g is higher than the openList node's g
-                continue to beginning of for loop
-        // Add the child to the openList
-        add the child to the openList
-"""
-     
  
 
   
   
-
-c1 = Car(0,0,2,2)
-c2 = Car(0,0,0,1)
 m = Map(200,200)
 
-g = Game(map=m, cars=[c1,c2])
+c1 = Car(80,40,2,2)
+c2 = Car(0,0,0,1)
 
-iterations = 100
+d1 = (160,110)
+d2 = (20,50)
+
+g = Main(map=m, cars=[c1,c2], dests=[d1,d2])
+
+
+iterations = 1000
 for _ in range(iterations):
   g.step()
   g.show()
   #time.sleep(0.5)
   
+
+
+
+  
+
+
 
 
 
